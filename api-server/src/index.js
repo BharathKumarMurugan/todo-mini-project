8e6f8a3aae38e3bd67539334bb7d066c41d912cb
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2025-06-05 00:37:26 +0530

    created a simple http server

--- api-server/src/index.js
+++ api-server/src/index.js
@@ -1,4 +1,43 @@
 import "dotenv/config";
 
-import express from "express";
+import express, { request, response } from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+import amqplib from "amqplib";
+
+const app = express();
+
+const PORT = process.env.API_PORT || 3000;
+
+let channel;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true })); // parse URL-encoded bodies
+app.use(bodyParser.json());
+app.use(express.json());
+
+async function connectToRabbitMQ() {
+  try {
+    const conn = await amqplib.connect(process.env.QUEUE_URL);
+    channel = await conn.createChannel();
+    await channel.assertQueue(process.env.QUEUE_NAME, { durable: false });
+    console.log("Connected to Queue server");
+  } catch (err) {
+    console.error("Failed to connect to Queue server: ", err);
+    process.exit(1);
+  }
+}
+
+app.get("/", (req, res) => {
+  const message = {
+    message: "Hello from API server",
+    status: "success",
+  };
+  res.status(200).json(message);
+});
+
+// connectToRabbitMQ().then(() => {
+app.listen(PORT, () => {
+  console.log(`API server is running on port ${PORT}`);
+});
+// });
