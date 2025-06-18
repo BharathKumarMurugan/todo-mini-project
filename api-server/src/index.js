4b940e86414fb9b6dc39a8eb3fff11f00476bf56
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2025-06-18 08:32:09 +0530

    updated the API server with logger

--- api-server/src/index.js
+++ api-server/src/index.js
@@ -4,6 +4,7 @@ import express, { request, response } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import amqplib from "amqplib";
+import { logger } from "./utils/logger.js";
 
 const app = express();
 
@@ -16,17 +17,36 @@ app.use(bodyParser.urlencoded({ extended: true })); // parse URL-encoded bodies
 app.use(bodyParser.json());
 app.use(express.json());
 
+/**
+ * Connects to RabbitMQ server and sets up the channel
+ */
 async function connectToRabbitMQ() {
   try {
     const conn = await amqplib.connect(process.env.QUEUE_URL);
     channel = await conn.createChannel();
     await channel.assertQueue(process.env.QUEUE_NAME, { durable: false });
-    console.log("Connected to Queue server");
+    logger.info("Connected to Queue server");
   } catch (err) {
-    console.error("Failed to connect to Queue server: ", err);
+    logger.error("Failed to connect to Queue server: ", err);
     process.exit(1);
   }
 }
+
+/**
+ * Middleware to handle logs for every API request
+ * Logs the request method, URL, IP address, status code,response time and user agent
+ */
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    logger.info(`API Request: ${req.method} ${req.originalUrl}`, {
+      ip: req.ip,
+      statusCode: res.statusCode,
+      responseTimeMs: res.get("X-Response-Time"),
+      userAgent: req.get("User-Agent"),
+    });
+  });
+  next();
+});
 
 app.get("/", (req, res) => {
   const message = {
@@ -36,8 +56,16 @@ app.get("/", (req, res) => {
   res.status(200).json(message);
 });
 
+app.get("/health", (req, res) => {
+  const message = {
+    message: "API server is healthy",
+    status: "success",
+  };
+  res.status(200).json(message);
+});
+
 // connectToRabbitMQ().then(() => {
 app.listen(PORT, () => {
-  console.log(`API server is running on port ${PORT}`);
+  logger.info(`API server is running on port ${PORT}`);
 });
 // });
---
+++ api-server/src/utils/logger.js
@@ -0,0 +1,21 @@
+import winston from "winston";
+
+const { createLogger, format, transports } = winston;
+const { combine, timestamp } = format;
+
+export const logger = createLogger({
+  level: "info",
+  format: combine(timestamp(), format.json()),
+  transports: [
+    new transports.File({ filename: "api-server.log" }), // combined log file
+    new transports.File({ filename: "api-server-error.log", level: "error" }), // api server error log file
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new transports.Console({
+      format: combine(format.colorize(), format.simple()),
+    })
+  );
+}
