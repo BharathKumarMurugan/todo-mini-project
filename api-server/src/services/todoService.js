4369a6d122f56d65b68feeec98d273aea0335c02
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2025-06-18 23:06:17 +0530

    updated with additional APIs

--- api-server/src/services/todoService.js
+++ api-server/src/services/todoService.js
@@ -4,8 +4,9 @@
 
 import { publishToQueue } from "../utils/queue";
 import { logger } from "../utils/logger";
+import { v4 as uuidv4 } from "uuid";
 
-exports.queueNewTodo = async (taskData, user) => {
+export const queueNewTodo = async (taskData, user) => {
   const message = {
     action: "create",
     task: taskData,
@@ -14,16 +15,68 @@ exports.queueNewTodo = async (taskData, user) => {
       email: user.email,
     },
     metadata: {
-      messageId: "",
+      messageId: uuidv4(),
       createdAt: new Date().toISOString(),
       source: "api-server",
     },
   };
-
   await publishToQueue(message);
   return {
     status: "success",
     message: "Task queued successfully",
-    tasId: taskData.id,
+    taskId: taskData.id,
   };
+};
+
+export const queueUpdateTodo = async (taskId, updatedTask, user) => {
+  const message = {
+    action: "update",
+    taskId,
+    updatedTask,
+    user: {
+      userId: user.id,
+      email: user.email,
+    },
+    metadata: {
+      messageId: uuidv4(),
+      createdAt: new Date().toISOString(),
+      source: "api-server",
+    },
+  };
+  await publishToQueue(message);
+  return {
+    status: "success",
+    message: "Task queued successfully",
+    taskId: taskData.id,
+  };
+};
+
+export const queueDeleteTodo = async (taskId, user) => {
+  const message = {
+    action: "delete",
+    taskId,
+    user: {
+      userId: user.id,
+      email: user.email,
+    },
+    metadata: {
+      messageId: uuidv4(),
+      createdAt: new Date().toISOString(),
+      source: "api-server",
+    },
+  };
+  await publishToQueue(message);
+  return {
+    status: "success",
+    message: "Task queued successfully",
+    taskId: taskData.id,
+  };
+};
+
+export const getTodos = async (user) => {
+  return await todoModel.find({ userId: user.id }).exec();
+};
+
+export const getTodoById = async (taskId, user) => {
+  return await todoModel.findById(taskId, { userId: user.id }).exec();
 };
