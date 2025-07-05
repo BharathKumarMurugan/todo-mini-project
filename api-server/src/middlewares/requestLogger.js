fc5f7f720d759342760e10498f0ebcc90863ec3c
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2025-07-05 18:15:56 +0530

    updated with response time return

--- api-server/src/middlewares/requestLogger.js
+++ api-server/src/middlewares/requestLogger.js
@@ -5,11 +5,15 @@
 import logger from "../utils/logger.js";
 
 export default function requestLogger(req, res, next) {
+  const start = process.hrtime();
+
   res.on("finish", () => {
+    const diff = process.hrtime(start);
+    const responseTimeMs = (diff[0] * 1e9 + diff[1]) / 1e6;
     logger.info(`API Request: ${req.method} ${req.originalUrl}`, {
       ip: req.ip,
       statusCode: res.statusCode,
-      responseTimeMs: res.get("X-Response-Time"),
+      responseTimeMs: responseTimeMs.toFixed(2),
       userAgent: req.get("User-Agent"),
     });
   });
