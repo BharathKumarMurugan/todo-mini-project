7600e0e718f126891b6f2630ecf35218d0bac56e
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2025-07-05 18:18:01 +0530

    updated db connection

--- api-server/src/utils/db.js
+++ api-server/src/utils/db.js
@@ -8,6 +8,15 @@ export async function connectToDB() {
   try {
     const connection = mongoose.connect(MONGODB_URI);
     logger.info(`Connected to Database on port ${PORT}`);
+    mongoose.connection.on("error", (err) => {
+      logger.error("Mongoose connection error:", err);
+    });
+    mongoose.connection.on("disconnected", () => {
+      logger.warn("Mongoose connection disconnected.");
+    });
+    mongoose.connection.on("reconnected", () => {
+      logger.info("Mongoose connection reconnected.");
+    });
     return connection;
   } catch (err) {
     logger.error("Failed to connect to Database: ", err);
