2a4763efa70f2534ec9e63c90929c17a41abf531
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2025-07-15 16:46:14 +0530

    updated logger format and log file path

--- api-server/src/utils/logger.js
+++ api-server/src/utils/logger.js
@@ -1,14 +1,29 @@
 import winston from "winston";
+import path from "path";
 
 const { createLogger, format, transports } = winston;
-const { combine, timestamp } = format;
+const { combine, timestamp, printf } = format;
+
+const jsonLogFormat = printf(({ level, message, timestamp, ...metadata }) => {
+  const logEntry = {
+    timestamp,
+    severity: level.toUpperCase(),
+    message,
+    ...metadata,
+  };
+  return JSON.stringify(logEntry);
+});
+
+const logDir = "/app/logs";
+const apiServerLogFile = "api-server.log";
+const apiServerErrorLogFile = "api-server-error.log";
 
 const logger = createLogger({
   level: "info",
-  format: combine(timestamp(), format.json()),
+  format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss.SSSZ" }), jsonLogFormat),
   transports: [
-    new transports.File({ filename: "api-server.log" }), // combined log file
-    new transports.File({ filename: "api-server-error.log", level: "error" }), // api server error log file
+    new transports.File({ filename: path.join(logDir, apiServerLogFile) }), // combined log file
+    new transports.File({ filename: path.join(logDir, apiServerErrorLogFile), level: "error" }), // api server error log file
   ],
 });
 
--- worker-node/app/logger.py
+++ worker-node/app/logger.py
@@ -1,12 +1,17 @@
 import logging
 import os
-
 from pythonjsonlogger import jsonlogger
 
 logger = logging.getLogger("worker-node")
 
 if not logger.handlers:
-    handler = logging.StreamHandler()
+    log_file = "worker-node.log"
+    log_dir = "/app/logs"
+
+    os.makedirs(log_dir, exist_ok=True)
+    log_path = os.path.join(log_dir, log_file)
+
+    handler = logging.FileHandler(log_path)
     log_level_str= os.getenv("LOG_LEVEL", "INFO").upper()
     log_level = getattr(logging, log_level_str, logging.INFO)
     logger.setLevel(log_level)
@@ -25,5 +30,12 @@ if not logger.handlers:
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
+    if os.getenv("LOG_TO_CONSOLE", "true").lower() == "true":
+        console_handler = logging.StreamHandler()
+        console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+        console_handler.setFormatter(console_formatter)
+        logger.addHandler(console_handler)
+
+logger.info(f"Worker-node logger initialized. Logging to {log_path}")
 # logger.info("logger initialized")
 # logger.warning("This is a warning message")
